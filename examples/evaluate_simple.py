6aa1d52dd667f6a74c8db2f73709b6f84073f32d
grzegorz <grzegorzklimek77@gmail.com>
2021-12-23 16:12:42 +0100

    evaluate models with simple test

--- examples/evaluate_simple.py
+++ examples/evaluate_simple.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-from rlcard.agents.cfr_agent import CFRAgent
+from rlcard.agents import CFRAgent, RandomAgent
 import datetime
 
 import rlcard
@@ -9,21 +9,28 @@ from rlcard.utils import  tournament, Logger
 
 def testModel(args):
     env = rlcard.make('leduc-holdem')
-    default_model = models.load('leduc-holdem-cfr').agents[0]
-    cfr_agent = CFRAgent(env, os.path.join(args.model_path, args.model_name))
-    cfr_agent.load()
-    env.set_agents([default_model, cfr_agent])
+    # comparison_model = RandomAgent(num_actions=env.num_actions)
+    comparison_agent = models.load('leduc-holdem-cfr').agents[0]
+    tested_agent = CFRAgent(env, os.path.join(args.model_path, args.model_name))
+    tested_agent.load()
+    env.set_agents([ comparison_agent, tested_agent])
     with Logger(args.log_dir) as logger:
-        logger.log_performance(env.timestep, tournament(env, args.num_eval_games)[0])
+        rewards = tournament(env, args.num_eval_games)
+        print(args.first_agent_name)
+        logger.log_performance(env.timestep, rewards[0])
+        print(args.second_agent_name)
+        logger.log_performance(env.timestep, rewards[1])
 
 
 if __name__ == '__main__':
     today_date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     parser = argparse.ArgumentParser("CFR example in RLCard")
     parser.add_argument('--num_eval_games', type=int, default=1000)
-    parser.add_argument('--model_path', type=str, default='../experiments/leduc_holdem_cfr_result/')
+    parser.add_argument('--model_path', type=str, default='experiments/leduc_holdem_cfr_result/')
     parser.add_argument('--model_name', type=str, default='cfr_model')
     parser.add_argument('--log_dir', type=str, default='experiments/leduc_holdem_compare_result/{}'.format(today_date))
+    parser.add_argument('--first_agent_name', type=str, default='random agent')
+    parser.add_argument('--second_agent_name', type=str, default='trained cfr agent')
 
     args = parser.parse_args()
 
